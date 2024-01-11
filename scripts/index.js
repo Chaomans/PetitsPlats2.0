62c4aeda6a9613c1564083abfc65f8b5c46b0faa
Chaomans <chaomans@gmail.com>
2024-01-11 14:19:00 +0100

    prevent page reload

--- scripts/index.js
+++ scripts/index.js
@@ -39,6 +39,11 @@ const init = async () => {
   displayRecipes(recipes);
   storeSearchedRecipes(recipes);
 
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
   const btnDisplayMore = document.querySelector(".more");
   btnDisplayMore.addEventListener("click", () => {
     const limit = +document.querySelector(".more").getAttribute("data-limit");
